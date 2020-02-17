e66f1e781f161faac784fb4d720def3e0b92f5cc
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2020-02-16 22:13:48 -0500

    Draft of Problem 73

---
+++ etc/problem73.py
@@ -0,0 +1,16 @@
+
+# Too slow right now. What optimization am I missing?
+
+import math
+
+total = 0
+
+for d in range(4, 12001):
+    n0 = math.floor(d / 3)
+    n1 = math.ceil(d / 2)
+    print(d, n0, n1)
+    for n in range(n0 + 1, n1):
+        if math.gcd(n, d) == 1:
+            total += 1
+
+print(total)
--- etc/smgen.lisp
+++ etc/smgen.lisp
@@ -13,7 +13,13 @@
     (nth "aA" 2 1)
     (range "nR" 2 1)
     (equal? "eQ" 2 1)
-    (dup "dU" 1 2)))
+    (dup "dU" 1 2)
+    (div "nD" 2 1)
+    (mod "NmO" 2 1)
+    (floor "nF" 1 1)
+    (ceil "nC" 1 1)
+    (not "nO" 1 1)
+    (wrap "wR" 1 1)))
 
 (defparameter *vars*
   '())
@@ -83,6 +89,11 @@
                (let ((literal (translate-literal literal))
                      (var (translate-var var)))
                  `(,var ,literal "*"))))
+      ;; (move src dest)
+      (move (destructuring-bind (src dest) (rest line)
+              (let ((src (translate-var src))
+                    (dest (translate-var dest)))
+                `(,src "#" ,dest "*"))))
       ;; (map var array out-var block)
       (map (destructuring-bind (var array out-var block) (rest line)
              (let ((var (translate-var var)))
@@ -112,6 +123,15 @@
                false-block
                (translate-input cond)
                '("bI")))))
+      ;; (while cond var body)
+      (while (destructuring-bind (cond var body) (rest line)
+               (let ((var (translate-var var)))
+                 (let ((cond (translate-block cond :suffix `(,var "#")))
+                       (body (translate-block body)))
+                   (append
+                    body
+                    cond
+                    '("bW"))))))
       (t (destructuring-bind (name inputs outputs) line
            (let ((inputs (mapcar #'translate-arg inputs))
                  (outputs (mapcar #'translate-var outputs)))
@@ -143,15 +163,72 @@
     '("(" "@" "(")
     (translate-lines lines))))
 
+;; (format t "~A~%"
+;;         (full-translate
+;;          '((store 1 total)
+;;            (while ((incr (total) (total))
+;;                    (dup (total) (total total1))
+;;                    (equal? (total1 10) (c))
+;;                    (not (c) (c)))
+;;                   c
+;;                   ((dup (total) (total total2))
+;;                    (to-string (total2) (total2))
+;;                    (print (total2) ()))))))
+
+;; ///// gcd part is not efficient enough, maybe hand-write that
+;; section if possible?
 (format t "~A~%"
         (full-translate
-         '((range (0 1000001) (phi))
-           (range (2 1000001) (iter))
-           (each index iter
-                 ((nth (phi index) (phi[index]))
-                  (equal? (phi[index] index) (cond))
-                  (if cond
-                      ((dup (index) (index jndex)))
-                    ())))
-           (to-string (963) (aaa))
-           (print (aaa) ()))))
+         '((store 0 total)
+           (range (4 12001) (iter))
+           (each d iter
+                 ((dup (d) (d d1))
+                  (dup (d1) (d1 d2))
+                  (div (d1 3) (n0))
+                  (div (d2 2) (n1))
+                  (floor (n0) (n0))
+                  (ceil (n1) (n1))
+                  (incr (n0) (n0))
+                  (range (n0 n1) (iter1))
+
+                  (dup (d) (d aaa))
+                  (mod (aaa 100) (bbb))
+                  (equal? (bbb 0) (bbb))
+                  (if bbb
+                      ((dup (d) (d aaa))
+                       (to-string (aaa) (aaa))
+                       (print (aaa) ())
+                       (wrap (13) (aaa))
+                       (print (aaa) ())
+                       (wrap (10) (aaa))
+                       (print (aaa) ()))
+                    ())
+
+                  (each n iter1
+                        ;; gcd calculation
+                        ((while ((dup (d) (d dzero))
+                                 (equal? (dzero 0) (cond))
+                                 (not (cond) (cond)))
+                                cond
+                                ((dup (d) (t0 t1))
+                                 (mod (n t0) (d))
+                                 (move t1 n)))
+                         (equal? (n 1) (eq))
+                         (if eq
+                             ((incr (total) (total)))
+                           ())))))
+           (to-string (total) (total))
+           (print (total) ()))))
+
+;; (format t "~A~%"
+;;         (full-translate
+;;          '((range (0 1000001) (phi))
+;;            (range (2 1000001) (iter))
+;;            (each index iter
+;;                  ((nth (phi index) (phi[index]))
+;;                   (equal? (phi[index] index) (cond))
+;;                   (if cond
+;;                       ((dup (index) (index jndex)))
+;;                     ())))
+;;            (to-string (963) (aaa))
+;;            (print (aaa) ()))))
