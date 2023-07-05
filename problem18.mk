ed74c62eca6cf4dc2b8b95a8518d750b8c9eb5cb
Silvio Mayolo <mercerenies@comcast.net>
2023-07-04 20:13:39 -0400

    Problem 18 (increase compatibility and remove a hack)

--- problem18.mk
+++ problem18.mk
@@ -15,8 +15,9 @@ from_num=$(if $(subst 0,,$1),$(call _from_num,$1,),)
 rows=14 13 12 11 10 9 8 7 6 5 4 3 2 1
 cols=1 2 3 4 5 6 7 8 9 10 11 12 13 14
 
-SP:=
-SP+=
+define SP
+ 
+endef
 
 collection=$(foreach var,$(subst ;,$(SP),$(word $(words $(data)),$(data))),$(subst $(SP),;,$(call from_num,$(var))))
 
