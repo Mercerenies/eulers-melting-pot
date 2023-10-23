4c11be671b19078f6a5b738e4981d38db1cd8c1f
Silvio Mayolo <mercerenies@comcast.net>
2023-10-23 17:07:49 -0400

    Powerful bit manipulations

--- etc/problem167_2.zig
+++ etc/problem167_2.zig
@@ -27,6 +27,10 @@ inline fn setBitOfArray(values: []u64, index: u64, value: u1) void {
     setBitOf(&values[index / SIZEOF_U64], @truncate(index % SIZEOF_U64), value);
 }
 
+inline fn firstNOnes(count: u6) u64 {
+    return (@as(u64, 1) << count) - 1;
+}
+
 inline fn doUlamStepSlow(ulam_array: []u64, non_ulam_array: []u64, k: u64, i: u64) void {
     const j = k + i;
     const src_bit = bitOfArray(ulam_array, i);
@@ -64,12 +68,42 @@ inline fn doUlamSum(ulam_array: []u64, non_ulam_array: []u64, k: u64) void {
             non_ulam_array[j] = non_ulam_value;
             ulam_array[j] = (dest_value | src_value) & ~non_ulam_value;
         }
-        // Now finish the last few bits the slow way.
-        for (((upper_bound/SIZEOF_U64) * SIZEOF_U64) .. upper_bound) |i| {
-            doUlamStepSlow(ulam_array, non_ulam_array, k, i);
-        }
     } else {
-        doUlamSumSlow(ulam_array, non_ulam_array, k);
+        // Hard case: Not axis-aligned
+        const dest_shift_amount = @as(u6, @truncate(k % SIZEOF_U64));
+        const lower_dest_mask = firstNOnes(dest_shift_amount);
+        const upper_dest_mask = ~lower_dest_mask;
+        const src_shift_amount = @as(u6, @truncate(SIZEOF_U64 - (k % SIZEOF_U64)));
+        const lower_src_mask = firstNOnes(src_shift_amount);
+        const upper_src_mask = ~lower_src_mask;
+        for (0..(upper_bound/SIZEOF_U64)) |i| {
+            // Map lower of src onto upper of dest
+            {
+                const j = k / SIZEOF_U64 + i;
+                const src_value = (ulam_array[i] & lower_src_mask) << dest_shift_amount;
+                const dest_value = ulam_array[j] & upper_dest_mask;
+                var non_ulam_value = non_ulam_array[j] & upper_dest_mask;
+                non_ulam_value |= src_value & dest_value;
+                non_ulam_array[j] |= src_value & dest_value;
+                ulam_array[j] |= src_value;
+                ulam_array[j] &= ~non_ulam_value;
+            }
+            // Map upper of src onto lower of dest
+            {
+                const j = k / SIZEOF_U64 + i + 1;
+                const src_value = (ulam_array[i] & upper_src_mask) >> src_shift_amount;
+                const dest_value = ulam_array[j] & lower_dest_mask;
+                var non_ulam_value = non_ulam_array[j] & lower_dest_mask;
+                non_ulam_value |= src_value & dest_value;
+                non_ulam_array[j] |= src_value & dest_value;
+                ulam_array[j] |= src_value;
+                ulam_array[j] &= ~non_ulam_value;
+            }
+        }
+    }
+    // Now finish the last few bits the slow way.
+    for (((upper_bound/SIZEOF_U64) * SIZEOF_U64) .. upper_bound) |i| {
+        doUlamStepSlow(ulam_array, non_ulam_array, k, i);
     }
 }
 
@@ -124,13 +158,14 @@ pub fn main() !void {
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    const bits = try generateUlamBits(allocator, 1, 2, 2500);
+    //const bits = try generateUlamBits(allocator, 1, 2, 40000);
+    const bits = try generateUlamBits(allocator, 2, 5, 20000);
     const values = try compileUlamValues(allocator, bits);
     //std.debug.print("{any}\n", .{values});
     //std.debug.print("{any}\n", .{bits});
     std.debug.print("first few = {any}\n", .{values.items[0..100]});
     std.debug.print("length = {any}\n", .{values.items.len});
 
-    std.debug.assert(std.mem.eql(u64, values.items[0..59], &[_]u64{1, 2, 3, 4, 6, 8, 11, 13, 16, 18, 26, 28, 36, 38, 47, 48, 53, 57, 62, 69, 72, 77, 82, 87, 97, 99, 102, 106, 114, 126, 131, 138, 145, 148, 155, 175, 177, 180, 182, 189, 197, 206, 209, 219, 221, 236, 238, 241, 243, 253, 258, 260, 273, 282, 309, 316, 319, 324, 339}));
-    std.debug.assert(values.items[9999] == 132788);
+    //std.debug.assert(std.mem.eql(u64, values.items[0..59], &[_]u64{1, 2, 3, 4, 6, 8, 11, 13, 16, 18, 26, 28, 36, 38, 47, 48, 53, 57, 62, 69, 72, 77, 82, 87, 97, 99, 102, 106, 114, 126, 131, 138, 145, 148, 155, 175, 177, 180, 182, 189, 197, 206, 209, 219, 221, 236, 238, 241, 243, 253, 258, 260, 273, 282, 309, 316, 319, 324, 339}));
+    //std.debug.assert(values.items[9999] == 132788);
 }
