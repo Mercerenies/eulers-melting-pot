bec636bb43778c28bef55c8cce23a7536c397ebb
Silvio Mayolo <mercerenies@comcast.net>
2023-10-23 16:42:02 -0400

    Some optimizations for axis-aligned case

--- etc/problem167_2.zig
+++ etc/problem167_2.zig
@@ -27,17 +27,21 @@ inline fn setBitOfArray(values: []u64, index: u64, value: u1) void {
     setBitOf(&values[index / SIZEOF_U64], @truncate(index % SIZEOF_U64), value);
 }
 
+inline fn doUlamStepSlow(ulam_array: []u64, non_ulam_array: []u64, k: u64, i: u64) void {
+    const j = k + i;
+    const src_bit = bitOfArray(ulam_array, i);
+    const dest_bit = bitOfArray(ulam_array, j);
+    var non_ulam_bit = bitOfArray(non_ulam_array, j);
+    non_ulam_bit |= (src_bit & dest_bit);
+    setBitOfArray(non_ulam_array, j, non_ulam_bit);
+    setBitOfArray(ulam_array, j, (dest_bit | src_bit) & ~non_ulam_bit);
+}
+
 inline fn doUlamSumSlow(ulam_array: []u64, non_ulam_array: []u64, k: u64) void {
     // The slow version; do each bit individually.
     const upper_bound = @min(k, ulam_array.len * SIZEOF_U64 - k);
     for (1..upper_bound) |i| {
-        const j = k + i;
-        const src_bit = bitOfArray(ulam_array, i);
-        const dest_bit = bitOfArray(ulam_array, j);
-        var non_ulam_bit = bitOfArray(non_ulam_array, j);
-        non_ulam_bit |= (src_bit & dest_bit);
-        setBitOfArray(non_ulam_array, j, non_ulam_bit);
-        setBitOfArray(ulam_array, j, (dest_bit | src_bit) & ~non_ulam_bit);
+        doUlamStepSlow(ulam_array, non_ulam_array, k, i);
     }
 }
 
@@ -49,17 +53,24 @@ inline fn doUlamSum(ulam_array: []u64, non_ulam_array: []u64, k: u64) void {
         return;
     }
 
-    // I want to go from 1 up to (exclusive) upper_bound. Start by
-    // getting aligned to a u64.
-    const init_span = SIZEOF_U64 - ((k + 1) % SIZEOF_U64);
-    _ = init_span;
-
-    //for (1..upper_bound) |i| {
-    //    const j = k + i;
-    //    non_ulam_array[j] = non_ulam_array[j] or (ulam_array[i] and ulam_array[j]);
-    //    ulam_array[j] = (ulam_array[j] or ulam_array[i]) and !non_ulam_array[j];
-    //}
-    doUlamSumSlow(ulam_array, non_ulam_array, k);
+    if (k % SIZEOF_U64 == 0) {
+        // Easy case: We're axis aligned
+        for (0..(upper_bound/SIZEOF_U64)) |i| {
+            const j = k / SIZEOF_U64 + i;
+            const src_value = ulam_array[i];
+            const dest_value = ulam_array[j];
+            var non_ulam_value = non_ulam_array[j];
+            non_ulam_value |= src_value & dest_value;
+            non_ulam_array[j] = non_ulam_value;
+            ulam_array[j] = (dest_value | src_value) & ~non_ulam_value;
+        }
+        // Now finish the last few bits the slow way.
+        for (((upper_bound/SIZEOF_U64) * SIZEOF_U64) .. upper_bound) |i| {
+            doUlamStepSlow(ulam_array, non_ulam_array, k, i);
+        }
+    } else {
+        doUlamSumSlow(ulam_array, non_ulam_array, k);
+    }
 }
 
 fn generateUlamBits(allocator: std.mem.Allocator, a: u64, b: u64, length: usize) AllocationError![]u64 {
@@ -117,6 +128,7 @@ pub fn main() !void {
     const values = try compileUlamValues(allocator, bits);
     //std.debug.print("{any}\n", .{values});
     //std.debug.print("{any}\n", .{bits});
+    std.debug.print("first few = {any}\n", .{values.items[0..100]});
     std.debug.print("length = {any}\n", .{values.items.len});
 
     std.debug.assert(std.mem.eql(u64, values.items[0..59], &[_]u64{1, 2, 3, 4, 6, 8, 11, 13, 16, 18, 26, 28, 36, 38, 47, 48, 53, 57, 62, 69, 72, 77, 82, 87, 97, 99, 102, 106, 114, 126, 131, 138, 145, 148, 155, 175, 177, 180, 182, 189, 197, 206, 209, 219, 221, 236, 238, 241, 243, 253, 258, 260, 273, 282, 309, 316, 319, 324, 339}));
