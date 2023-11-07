6620f02a198f9ae6bff7060c0e78fc3e5f477a8b
Silvio Mayolo <mercerenies@comcast.net>
2023-11-06 20:42:20 -0500

    Problem 22 (Improved formatting)

--- problem22.exs
+++ problem22.exs
@@ -1,14 +1,13 @@
+
 import Enum
 
 case File.read "./files/p022_names.txt" do
   {:ok, var} ->
-    list = sort(String.split(String.replace(var, "\"", ""), ","))
-    result = map(with_index(list, 1),
-                  fn {val, ind} ->
-                    ind * reduce(map(to_char_list(val), fn cp ->
-                                                          cp - 64
-                                    end), &+/2)
-                  end)
+    list = var |> String.replace("\"", "") |> String.split(",") |> sort
+    result = with_index(list, 1) |> map(fn {word, index} ->
+      alphabetical_value = to_charlist(word) |> map(fn cp -> cp - 64 end) |> reduce(&+/2)
+      index * alphabetical_value
+    end)
     IO.puts reduce(result, &+/2)
   _ -> :no
 end
