269823aa47f3370de33ba72d8f778aa620811eba
Silvio Mayolo <mercerenies@comcast.net>
2024-02-26 17:19:45 -0500

    Problem 26 (Fixed sequencing)

--- problem26.sh
+++ problem26.sh
@@ -7,12 +7,11 @@ for var in `seq 1 999`; do
     into=1
     while [[ ! "$denoms" =~ "b"$into"e" ]]; do
         denoms=$denoms"b"$into"e"
-        (( into=$into * 10 ))
         (( into=$into % $var ))
+        (( into=$into * 10 ))
     done
     answer=`echo $denoms | grep -o "b"$into"e.*$"`
     answer=`echo $answer | tr e "\n" | wc -l`
-    (( answer=$answer - 1 ))
     if (( $answer > $maxlen )); then
         max=$var
         maxlen=$answer
