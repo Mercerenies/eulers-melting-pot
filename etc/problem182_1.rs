870a6d4319ca0875bed6c7ab86baaee1504531eb
Silvio Mayolo <mercerenies@comcast.net>
2024-05-04 11:53:32 -0400

    Working draft of 182

--- etc/problem182_1.rs
+++ etc/problem182_1.rs
@@ -1,6 +1,4 @@
 
-use std::collections::HashSet;
-
 fn gcd(mut a: i64, mut b: i64) -> i64 {
   while b != 0 {
     let t = b;
@@ -61,33 +59,44 @@ fn get_orders(n: i64) -> Vec<i64> {
 }
 
 fn main() {
-  let p = 19; // 1009;
-  let q = 37; // 3643;
+  let p = 1009;
+  let q = 3643;
   let limit = p * q;
   let phi = (p - 1) * (q - 1);
   let orders = get_orders(limit);
-  let orders: HashSet<_> = orders.into_iter().skip(1).collect();
-  // For each order N in the orders set and any integer k, we want to
-  // avoid e = k N + 1, since that will produce at least one
-  // nontrivial unconcealed message. We ignore anything of order 1,
-  // since any message with multiplicative order 1 will ALWAYS be an
-  // unconcealed message, regardless of our choice of e.
-  let mut desired_e_values_bitmask = vec![true; phi as usize];
-  desired_e_values_bitmask[0] = false;
-  desired_e_values_bitmask[1] = false;
-  for order in orders {
+  // If m has order N, then for any integer k, e = k N + 1 will leave
+  // m unconcealed. Mark all of the e values for which this is true
+  // and keep a count.
+  let mut unconcealed_per_e = vec![0i64; phi as usize];
+  // 0 and 1 are not valid solutions, so don't use them.
+  unconcealed_per_e[0] = i64::MAX;
+  unconcealed_per_e[1] = i64::MAX;
+  for order in orders.into_iter().skip(1) {
     if order == 1 {
-      continue; // Skip order 1; as we can never satisfy those values
+      // Skip order 1; as we can never satisfy those values. Doing
+      // this doesn't change the answer but does speed up the
+      // computation.
+      continue;
     }
     let mut i = order;
     while i + 1 < phi {
-      desired_e_values_bitmask[(i + 1) as usize] = false;
+      unconcealed_per_e[(i + 1) as usize] += 1;
       i += order;
     }
   }
+
+  // Disable all of the e values which are NOT coprime to phi.
+  for e in 2..phi {
+    if gcd(e, phi) != 1 {
+      unconcealed_per_e[e as usize] = i64::MAX;
+    }
+  }
+
+  // Find the minimum, and count all e's that match it.
+  let min = *unconcealed_per_e.iter().min().unwrap();
   let mut sum = 0;
-  for (e, mask) in desired_e_values_bitmask.into_iter().enumerate() {
-    if gcd(e as i64, phi) == 1 && mask {
+  for (e, unconcealed_count) in unconcealed_per_e.into_iter().enumerate() {
+    if unconcealed_count == min {
       sum += e;
     }
   }
