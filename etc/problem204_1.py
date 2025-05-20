16f94f31878b603a960e856f898a015ea430a6e0
Silvio Mayolo <mercerenies@comcast.net>
2025-05-20 10:29:31 -0400

    additional 204 draft work

---
+++ etc/problem204_1.py
@@ -0,0 +1,21 @@
+
+PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
+          41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
+LIMIT = int(1e9)
+
+
+def enumerate_hamming_numbers(base: int, index: int) -> int:
+    if base > LIMIT:
+        return 0
+    if index >= len(PRIMES):
+        return 1
+    total = 0
+    while base <= LIMIT:
+        total += enumerate_hamming_numbers(base, index + 1)
+        base *= PRIMES[index]
+    return total
+
+
+if __name__ == "__main__":
+    print(enumerate_hamming_numbers(base=1, index=0))
