c7845d4918a962a50975882db434eb65164bc103
Mercerenies <mercerenies@gmail.com>
2026-07-03 16:01:56 -0400

    more setup work

--- etc/problem209.asm
+++ etc/problem209.asm
@@ -5,6 +5,20 @@ ORG     $1000
 ;; registers. Thus, I need 64 triple words, or 384 bytes.
 FIBO:   ds.b 384
 
+DD0:    ds.w 1
+DD1:    ds.w 1
+DD2:    ds.w 1
+DD3:    ds.w 1
+DD4:    ds.w 1
+DD5:    ds.w 1
+
+DD05:   ds.w 2
+DD15:   ds.w 2
+DD25:   ds.w 2
+DD14:   ds.w 2
+DD24:   ds.w 2
+DD23:   ds.w 2
+
 MAIN:
   ;; Initialize Fibonacci Sequence (current fibo values are d0;d1;d2 and d3;d4;d5)
   MOVE.W #0, d0
@@ -43,7 +57,7 @@ FIBO_LOOP:
   MOVE.W #0, d6
   MOVE.W #0, d0
   MOVE.W #0, d1
-  MOVE.W #0, d2
+  MOVE.W #1, d2
 MAIN_LOOP:
   MOVEA.W #0, a0    ; cycle_length = 0
   MOVE.W d6, d7     ; k = i
@@ -65,14 +79,14 @@ K_LOOP:
   LSL.W #1, d7      ; k *= 2
   ADD.W d5, d7      ; k += tmp
   CMP.W d7, d6      ; cmp = (k <=> i)
-  BCS LOOP_END      ; skip if i < k
+  BHI LOOP_END      ; skip if i < k
   BNE K_LOOP        ; continue if i != k
   CMPA.W #2, a0     ; cmp = (2 <=> cycle_length)
-  BEQ THREE
-  BHI FIBO
+  BEQ THREE_CASE
+  BPL FIBO_CASE
   JMP LOOP_END
 
-THREE:
+THREE_CASE:
   ;; total *= 3
   MULU #3, d2
   MULU #3, d1
@@ -91,10 +105,10 @@ THREE:
   AND.L #$FFFF, d2
   JMP LOOP_END
 
-FIBO:
+FIBO_CASE:
   ;; d3;d4;d5 will hold our Fibonacci sum
   LEA.L FIBO, a1
-  MOVE.W #5, d7
+  MOVE.W a0, d7
   MULU #6, d7
   ADDA.W d7, a1
   ;; a1 is now the position of cycle_length in the array.
@@ -111,12 +125,135 @@ FIBO:
   ADDX.W a7, d4
   MOVEA.W -(a1), a7
   ADDX.W a7, d3
-  JMP LOOP_END
+
+  ;; Goal: Multiply d0;d1;d2 by d3;d4;d5 and put the result in
+  ;; d0;d1;d2. Do not touch d6 but everything else is fair game.
+
+  ;; Step 1: Store everything in memory (very inefficient, don't care)
+  LEA.L DD0, a0
+  MOVE.W d0, (a0)
+  LEA.L DD1, a0
+  MOVE.W d1, (a0)
+  LEA.L DD2, a0
+  MOVE.W d2, (a0)
+  LEA.L DD3, a0
+  MOVE.W d3, (a0)
+  LEA.L DD4, a0
+  MOVE.W d4, (a0)
+  LEA.L DD5, a0
+  MOVE.W d5, (a0)
+
+  ;; Step 2: Do each of the multiplications
+
+  ;; NOTE: Again, M68k supports indirects as the source arg of MULU,
+  ;; but our interpreter does not. So we're doing some shenanigans.
+
+  ;; d0*d5
+  LEA.L DD0, a0
+  MOVE.W (a0), d0
+  LEA.L DD5, a0
+  MOVE.W (a0), d7
+  MULU d7, d0
+  LEA.L DD05, a0
+  MOVE.L d0, (a0)
+
+  ;; d1*d5
+  LEA.L DD1, a0
+  MOVE.W (a0), d0
+  LEA.L DD5, a0
+  MOVE.W (a0), d7
+  MULU d7, d0
+  LEA.L DD15, a0
+  MOVE.L d0, (a0)
+
+  ;; d2*d5
+  LEA.L DD2, a0
+  MOVE.W (a0), d0
+  LEA.L DD5, a0
+  MOVE.W (a0), d7
+  MULU d7, d0
+  LEA.L DD25, a0
+  MOVE.L d0, (a0)
+
+  ;; d1*d4
+  LEA.L DD1, a0
+  MOVE.W (a0), d0
+  LEA.L DD4, a0
+  MOVE.W (a0), d7
+  MULU d7, d0
+  LEA.L DD14, a0
+  MOVE.L d0, (a0)
+
+  ;; d2*d4
+  LEA.L DD2, a0
+  MOVE.W (a0), d0
+  LEA.L DD4, a0
+  MOVE.W (a0), d7
+  MULU d7, d0
+  LEA.L DD24, a0
+  MOVE.L d0, (a0)
+
+  ;; d2*d3
+  LEA.L DD2, a0
+  MOVE.W (a0), d0
+  LEA.L DD3, a0
+  MOVE.W (a0), d7
+  MULU d7, d0
+  LEA.L DD23, a0
+  MOVE.L d0, (a0)
+
+  ;; Step 3: Add results together
+
+  ;; d2' = (d2d5)ˡ
+  LEA.L DD25, a0
+  MOVE.L (a0), d2
+  AND.L #$FFFF, d2
+
+  ;; d1' = (d2d5)ʰ + (d1d5)ˡ + (d2d4)ˡ [store final carry in d7]
+  MOVE.W #0, d7
+  LEA.L DD25, a0
+  MOVE.L (a0), d1
+  LSR.L #8, d1
+  LSR.L #8, d1
+  LEA.L DD15, a0
+  MOVE.L (a0), d3
+  AND.L #$FFFF, d3
+  ADD.W d3, d1
+  ADDX.W #0, d7
+  LEA.L DD24, a0
+  MOVE.L (a0), d3
+  AND.L #$FFFF, d3
+  ADD.W d3, d1
+  ADDX.W #0, d7
+
+  ;; d0' = (d1d5)ʰ + (d2d4)ʰ + (d0d5)ˡ + (d1d4)ˡ + (d2d3)ˡ + carry(d1') [carry is stored in d7]
+  LEA.L DD15, a0
+  MOVE.L (a0), d0
+  LSR.L #8, d0
+  LSR.L #8, d0
+  LEA.L DD24, a0
+  MOVE.L (a0), d3
+  LSR.L #8, d3
+  LSR.L #8, d3
+  ADD.W d3, d0
+  LEA.L DD05, a0
+  MOVE.L (a0), d3
+  AND.L #$FFFF, d3
+  ADD.W d3, d0
+  LEA.L DD14, a0
+  MOVE.L (a0), d3
+  AND.L #$FFFF, d3
+  ADD.W d3, d0
+  LEA.L DD23, a0
+  MOVE.L (a0), d3
+  AND.L #$FFFF, d3
+  ADD.W d3, d0
+  ADD.W d7, d0
 
 LOOP_END:
   ADDQ.W #1, d6     ; i += 1
   CMP.W #64, d6
-  JNE MAIN_LOOP
+  BNE MAIN_LOOP
 
   ;; Output in d0;d1;d2
 END
